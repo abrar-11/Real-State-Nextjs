e18fefed06a0409550bd5306f008a9d9f3c955ab
Abrar Ahmed <abrarahmed2185@gmail.com>
2022-07-28 00:13:31 +0500

    CLean UP

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
     console.log("inside")
    const {data} = await axios.get(url, {
       headers: {
-         "X-RapidAPI-Key": "17911df630msh5762bd5cd10aa51p1c70b9jsna4b24278ee58",
+         "X-RapidAPI-Key":    process.env.API_KEY,
          "X-RapidAPI-Host": "bayut.p.rapidapi.com",
       },
    });
