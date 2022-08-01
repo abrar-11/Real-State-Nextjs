85b59582edb2ac33a7cb448941b62270ab0bef00
Abrar Ahmed <abrarahmed2185@gmail.com>
2022-08-01 20:27:06 +0500

    minor bug fixes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,24 +6,24 @@ const Hero2 = () => {
       <div className="flex justify-between items-center flex-wrap my-20">
          {/* Content Starts */}
 
-         <div class="sm:text-center lg:text-left w-6/12">
-            <h1 class="font-extrabold text-gray-800   text-5xl">
+         <div className="sm:text-center lg:text-left w-6/12">
+            <h1 className="font-extrabold text-gray-800   text-5xl">
                Time to Meet Your
             </h1>
-            <h1 class=" font-extrabold text-emerald-600  text-6xl">
+            <h1 className=" font-extrabold text-emerald-600  text-6xl">
                New Home
             </h1>
-            <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-md sm:max-w-xl sm:mx-auto md:mt-5 md:text-md lg:mx-0">
+            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-md sm:max-w-xl sm:mx-auto md:mt-5 md:text-md lg:mx-0">
                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
                praesentium dolorem dolor iste eveniet cupiditate minima.
                Voluptas perspiciatis nisi aliquid, repudiandae, laborum quod
                dolor consequuntur sapiente, rem reprehenderit veniam culpa!
             </p>
-            <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-               <div class="mt-3 sm:mt-0 ">
+            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+               <div className="mt-3 sm:mt-0 ">
                   <a
                      href="#"
-                     class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-emerald-700 bg-emerald-200 hover:bg-emerald-300 md:py-4 md:text-sm md:px-10"
+                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-emerald-700 bg-emerald-200 hover:bg-emerald-300 md:py-4 md:text-sm md:px-10"
                   >
                      Explore Properties
                   </a>
@@ -32,13 +32,12 @@ const Hero2 = () => {
          </div>
 
          {/* Img Starts */}
-         <div class="lg:w-1/2">
+         <div className="lg:w-1/2">
             <Image
                src={Background}
-               class="h-full w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-               style={{
-                  clipPath: "polygon(19% 0, 100% 0%, 100% 99%, 0% 100%)",
-               }}
+               className="h-full w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+               style={{clipPath: "polygon(19% 0, 100% 0%, 100% 99%, 0% 100%)"}}
+               alt="Home"
             />
          </div>
       </div>
